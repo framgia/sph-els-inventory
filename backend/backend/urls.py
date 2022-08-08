87b3bce7fb16ec862db50383ad320056ff518b34
johnalbert.castillo <johnalbert.castillo@sun-asterisk.com>
2022-08-08 13:40:18 +0800

    [BE] Setup backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserView, 'user')
+router.register(r'reservations', views.ReservationView, 'reservation')
+router.register(r'rooms', views.RoomView, 'room')
+router.register(r'items', views.ItemView, 'item')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
