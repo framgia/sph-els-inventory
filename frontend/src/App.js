c7b0a79fb6f8d71acd30ceb457e28b7472595008
Armand-Catubig <armand.catubig@sun-asterisk.com>
2022-08-08 18:32:07 +0800

    Added Lines and Proper Spacing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import NavBar from "./components/navbar/";
 
-const App = () => {
-    return (
-        <div>
-            <NavBar/>
-        </div>
-    );
+const App = () => { 
+  return (
+    <div>
+      <NavBar/>
+    </div>
+  );
 }
-
 
 export default App;
--- frontend/src/components/navbar/index.js
+++ frontend/src/components/navbar/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <div className="ui secondary pointing menu">
-      <div className="item">Home Inventory System</div>
-      <div className="right menu">
-        <div className="item">Dashboard</div>
-        <div className="item">Register a User</div>
-        <div className="item">List of Users</div>
-      </div>
+  <div className="ui secondary pointing menu">
+    <div className="item">Home Inventory System</div>
+    <div className="right menu">
+    <div className="item">Dashboard</div>
+    <div className="item">Register a User</div>
+    <div className="item">List of Users</div>
     </div>
+  </div>
   );
 };
 
