e04e51167c6d29062b9f02e92e7b16c886f67a1b
Armand-Catubig <108708255+Armand-Catubig@users.noreply.github.com>
2022-08-09 11:45:36 +0800

    [FE] Create Nav Bar Component (#3)
    * [FE] Create Nav Bar Component
    
    * Removed unnecessary syntax
    
    * Applied changes and comments
    
    * Added Lines and Proper Spacing
    
    * Resolved Conflicts
    
    * Updated Spacing

---
+++ frontend/src/components/navbar/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div className="ui secondary pointing menu">
+      <div className="item">Home Inventory System</div>
+      <div className="right menu">
+        <div className="item">Dashboard</div>
+        <div className="item">Register a User</div>
+        <div className="item">List of Users</div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
