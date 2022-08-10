95c4923f87916251cf3a384bbb43d89062ab090b
Armand-Catubig <armand.catubig@sun-asterisk.com>
2022-08-10 11:20:28 +0800

    [FE] Layout for Edit User Page

---
+++ frontend/src/pages/edit-user-page/index.js
@@ -0,0 +1,28 @@
+import "./index.css";
+import React from "react";
+import NavBar from '../../components/navbar/index'
+
+const EditUserPage = () => {
+  return (
+    <div>
+      <NavBar/>
+      <div className="boxContainer">
+        <div>
+          <h2>Edit User Name</h2>
+          <form className="ui form">
+            <div className="field">
+              <label>New User Name</label>
+              <input type="text"/>
+            </div>
+          </form>
+          <br/>
+          <div>
+            <button className="button">Submit</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditUserPage;
