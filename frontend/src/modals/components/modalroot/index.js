5f49b506015e28c9131184a18cb7d4b484d9a0e0
Armand-Catubig <armand.catubig@sun-asterisk.com>
2022-08-11 14:56:53 +0800

    [FE] Create reusable modal component

---
+++ frontend/src/modals/components/modalroot/index.js
@@ -0,0 +1,33 @@
+import styles from "../../styles/modalrootmodule/index.module.css";
+import { useState, useEffect } from "react";
+import ModalService from "../../services/modalservices";
+
+export default function ModalRoot() {
+  const [modal, setModal] = useState({});
+
+  useEffect(() => {
+    ModalService.on("open", ({ component, props }) => {
+      setModal({
+        component,
+        props,
+        close: (value) => {
+          setModal({});
+        },
+      });
+    });
+  }, []);
+
+  const ModalComponent = modal.component ? modal.component : null;
+
+  return (
+    <section className={modal.component ? styles.modalRoot : ""}>
+      {ModalComponent && (
+        <ModalComponent
+          {...modal.props}
+          close={modal.close}
+          className={ModalComponent ? "d-block" : ""}
+        />
+      )}
+    </section>
+  );
+}
