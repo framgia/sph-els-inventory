2d8a094893854a5e90da2a885b7f17d1cc39e946
John-Renz <johnrenz.valencia@sun-asterisk.com>
2022-08-15 13:58:11 +0800

    [FE] Implement Edit User Confirmation Modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import WelcomePage from "./pages/welcome";
 import AddItemPage from "./pages/add-item";
-import NavBar from "./components/NavBar";
 import DashboardPage from "./pages/dashboard";
 import DisplayUsersPage from "./pages/display-users";
+import EditUserPage from "./pages/edit-user-page";
 
 const App = () => {
   return <WelcomePage />;
