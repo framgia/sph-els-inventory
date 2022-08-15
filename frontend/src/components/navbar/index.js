d5c07bbeb8a9d311796a8da5c79a574151937a07
krishabriones-sunasterisk <108708605+krishabriones-sunasterisk@users.noreply.github.com>
2022-08-15 18:12:19 +0800

    Added Page Routing in Welcome, Register, Dashboard, Display User page (#16)

--- frontend/src/components/navbar/index.js
+++ frontend/src/components/navbar/index.js
@@ -1,13 +1,43 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  const navigate = useNavigate();
   return (
     <div className="ui secondary pointing menu">
-      <div className="item">Home Inventory System</div>
+      <div
+        className="item"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Home Inventory System
+      </div>
       <div className="right menu">
-        <div className="item">Dashboard</div>
-        <div className="item">Register a User</div>
-        <div className="item">List of Users</div>
+        <div
+          className="item"
+          onClick={() => {
+            navigate("/dashboard");
+          }}
+        >
+          Dashboard
+        </div>
+        <div
+          className="item"
+          onClick={() => {
+            navigate("/register");
+          }}
+        >
+          Register a User
+        </div>
+        <div
+          className="item"
+          onClick={() => {
+            navigate("/display-users");
+          }}
+        >
+          List of Users
+        </div>
       </div>
     </div>
   );
--- inventory_backend/api/serializers.py
+++ inventory_backend/api/serializers.py
@@ -6,7 +6,7 @@ from .models import User, Reservation, Room, Item
 class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
-    fields = (['name'])
+    fields = (['id','name'])
 
 class ReservationSerializer(serializers.ModelSerializer):
   user= serializers.SlugRelatedField(
@@ -17,12 +17,12 @@ class ReservationSerializer(serializers.ModelSerializer):
   )
   class Meta:
     model = Reservation
-    fields = ('user', 'reserved_date_from', 'reserved_date_to') # '__all__'
+    fields = ('id','user', 'reserved_date_from', 'reserved_date_to') # '__all__'
 
 class RoomSerializer(serializers.ModelSerializer):
   class Meta:
     model = Room
-    fields = (['name'])
+    fields = (['id','name'])
 
 class ItemSerializer(serializers.ModelSerializer):
   room= serializers.SlugRelatedField(
@@ -34,4 +34,4 @@ class ItemSerializer(serializers.ModelSerializer):
  
   class Meta:
      model = Item
-     fields = ('name', 'reservation', 'room')
+     fields = ('id','name', 'reservation', 'room')
